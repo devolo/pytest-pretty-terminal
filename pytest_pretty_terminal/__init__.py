23e6c78195d1520c06ebb7fd2d3b23a942425e0e
Guido Schmitz <guido.schmitz@devolo.de>
2021-09-01 18:08:44 +0200

    Fix linting issues

--- pytest_pretty_terminal/__init__.py
+++ pytest_pretty_terminal/__init__.py
@@ -1,14 +1,19 @@
 """pytest plugin for generating prettier terminal output"""
 
+import shutil
 from importlib.metadata import PackageNotFoundError, version
 
 import pytest
 from _pytest.config import Config
 from _pytest.config.argparsing import Parser
 from _pytest.python import Function
+from _pytest.reports import TestReport
 from _pytest.runner import CallInfo
-from ._helpers import build_terminal_report, get_item_name_and_spec, get_status_color
+from pluggy.callers import _Result
+from py.io import TerminalWriter
+
 from ._pretty_terminal_reporter import PrettyTerminalReporter
+
 try:
     __version__ = version("pytest_pretty_terminal")
 except PackageNotFoundError:
@@ -16,24 +21,28 @@ except PackageNotFoundError:
     __version__ = "0.0.0"
 
 
-
 @pytest.hookimpl(hookwrapper=True, tryfirst=True)
-def pytest_runtest_makereport(item: Function, call: CallInfo):
-    """This is called at setup, run/call and teardown of test items.
-
-        Generates adaptavist test run results from test reports.
+def pytest_runtest_makereport(item: Function, call: CallInfo):  # pylint: disable=unused-argument
     """
-    outcome = yield
-        
-    report = outcome.get_result()
-    
+    This is called at setup, run/call and teardown of test items.
+    Collects used parameter and test's docstrings.
+
+    :param item: A Function item
+    :param call: The CallInfo for the phase
+    """
+    outcome: _Result = yield
+    report: TestReport = outcome.get_result()
     if hasattr(item, 'callspec'):
         report.user_properties.append(("params", item.callspec.params))
     report.user_properties.append(("docstr", item.obj.__doc__))
 
 
 def enable_terminal_report(config: Config):
-    """Enable terminal report."""
+    """
+    Enable terminal reporting.
+
+    :param config: The pytest config object
+    """
     terminalreporter = PrettyTerminalReporter(config)
     config.pluginmanager.register(terminalreporter, "pretty_terminal_reporter")
     pytest.reporter = config.pluginmanager.getplugin("pretty_terminal_reporter")
@@ -48,17 +57,19 @@ def enable_terminal_report(config: Config):
 
 
 def patch_terminal_size(config: Config):
-    """Patch terminal size."""
+    """
+    Patch terminal size.
+
+    :param config: The pytest config object
+    """
 
     # this function tries to fix the layout issue related to jenkins console
-    terminalreporter = config.pluginmanager.getplugin("terminalreporter_2")
-
+    terminalreporter = config.pluginmanager.getplugin("pretty_terminal_reporter")
     if not terminalreporter:
         return
 
-    tw = getattr(terminalreporter.tr, "_tw")
-
-    if not tw:
+    terminal_writer: TerminalWriter = config.get_terminal_writer()
+    if not terminal_writer:
         return
 
     try:
@@ -71,23 +82,23 @@ def patch_terminal_size(config: Config):
         default_width = 152
         default_height = 24
 
-    import shutil
     width, _ = shutil.get_terminal_size((default_width, default_height))
-    tw.fullwidth = width
-
+    terminal_writer.fullwidth = width
 
 
 @pytest.hookimpl(trylast=True)
 def pytest_configure(config: Config):
-    # init the terminal reporter
+    """
+    Perform initial configuration.
+
+    :param config: The pytest config object
+    """
     if not hasattr(config, "workerinput"):
         enable_terminal_report(config)
         if pytest.reporter:
             pytest.reporter.tr.section("ATM build meta data", bold=True)
-
             pytest.reporter.tr.line("build_usr: %s" % ("build_usr" or "unknown"))
     patch_terminal_size(config)
-    
 
 
 def import_module(module_name: str):
@@ -97,7 +108,7 @@ def import_module(module_name: str):
         return pytest.importorskip(module_name)
     except pytest.skip.Exception:
         return None
-    
+
 # if import_module("xdist"):
 #     @pytest.hookimpl(trylast=True)
 #     def pytest_configure_node(node: "WorkerController"):
@@ -106,106 +117,14 @@ def import_module(module_name: str):
 #             "dist": node.config.option.dist,
 #             "numprocesses": node.config.option.numprocesses
 #         }
-        
 
-    
+
 def pytest_addoption(parser: Parser):
-    """Add options to control plugin."""
+    """
+    Add options to control the plugin.
 
+    :param parser: Parser for command line arguments and ini-file values
+    """
     group = parser.getgroup("pretty-terminal")
     group.addoption("--pretty", action="store_true", dest="pretty", default=False,
                     help="Make pytest terminal output more readable (default: False)")
-
-# @pytest.hookimpl(tryfirst=True)
-# def pytest_runtest_setup(item):
-#     """This is called before calling the test item (i.e. before any parameter/fixture call).
-
-#         Used to skip test items dynamically (e.g. triggered by some other item or control function).
-#     """
-#     build_terminal_report(when="setup", item=item)
-
-#     skip_status = get_marker(item, "block") or get_marker(item, "skip")
-
-#     skip_reason = None
-
-#     if not skip_reason and skip_status:
-#         skip_reason = skip_status.kwargs.get("reason", None)
-#     if not skip_reason and pytest.test_result_data[item.fullname].get("blocked", None) is True:
-#         skip_reason = pytest.test_result_data[item.fullname].get("comment", None)
-
-#     if skip_reason:
-#         pytest.skip(msg=skip_reason)
-
-# @pytest.hookimpl(hookwrapper=True, tryfirst=True)
-# def pytest_runtest_makereport(item, call):
-#     """This is called at setup, run/call and teardown of test items.
-
-#         Generates adaptavist test run results from test reports.
-#     """
-#     outcome = yield
-
-#     report = outcome.get_result()
-
-#     report.item_info = {}
-#     report.item_info["atmcfg"] = {"project_key": pytest.project_key, "test_environment": pytest.test_environment, "test_plan_key": pytest.test_plan_key, "test_run_key": pytest.test_run_key}
-#     report.item_info["nodeid"] = get_item_nodeid(item)
-#     report.item_info["docstr"] = inspect.cleandoc(item.obj.__doc__ or "")
-
-#     skip_status = get_marker(item, "block") or get_marker(item, "skip")
-
-#     if call.when == "setup":
-#         if getattr(item.config.option, "adaptavist", False):
-#             # setup report only if adaptavist reporting is enabled
-#             setup_report(getattr(item.config, "workerinput", {}))
-#             report.item_info["atmcfg"] = {"project_key": pytest.project_key, "test_plan_key": pytest.test_plan_key, "test_run_key": pytest.test_run_key}
-
-#         if not call.excinfo and not skip_status and not pytest.test_result_data[item.fullname].get("blocked", None) is True:
-#             # no skipped or blocked methods to report
-#             return
-#     elif call.when != "call":
-#         return
-
-#     # if method was blocked dynamically (during call) an appropriate marker is used
-#     # to handle the reporting in the same way as for statically blocked methods
-#     # (status will be reported as "Blocked" with given comment in Adaptavist)
-#     if not skip_status and ((call.excinfo and call.excinfo.type in (pytest.block.Exception, pytest.skip.Exception)) or (not call.excinfo and pytest.test_result_data[item.fullname].get("blocked", None) is True)):
-#         reason = pytest.test_result_data[item.fullname].get("comment", None) or (str(call.excinfo.value).partition("\n")[0] if call.excinfo and call.excinfo.type in (pytest.block.Exception, pytest.skip.Exception) else None)
-#         skip_status = pytest.mark.block(reason=reason) if ((call.excinfo and call.excinfo.type is pytest.block.Exception) or pytest.test_result_data[item.fullname].get("blocked", None) is True) else pytest.mark.skip(reason=reason)
-#         if report.outcome != "skipped":
-#             report.outcome = "skipped"  # to mark this as SKIPPED in pytest reports
-#             report.longrepr = (__file__, getattr(sys, "_getframe")().f_lineno if hasattr(sys, "_getframe") else None, f"Skipped: {reason or 'blocked dynamically or partially'}")
-
-#     # report exceptions
-#     if call.excinfo:
-#         exc_info = build_exception_info(item.fullname, call.excinfo.type, call.excinfo.value, getattr(call.excinfo.traceback[-1], "_rawentry"))
-
-#         if exc_info and exc_info not in (pytest.test_result_data[item.fullname].get("comment", None) or ""):
-
-#             if (call.excinfo.type is not pytest.skip.Exception) and not skip_status:
-#                 pytest.test_result_data[item.fullname]["comment"] = "".join((pytest.test_result_data[item.fullname].get("comment", None) or "", html_row(False, exc_info)))
-
-#     # handling failed assumptions
-#     handle_failed_assumptions(item, call, report)
-
-#     build_report_description(item, call, report, skip_status)
-
-#     build_terminal_report(when="call", item=item, status=report.outcome if not skip_status else ("blocked" if skip_status.name == "block" else "skipped"))
-
-#     report.item_info["report"] = pytest.report[get_item_nodeid(item)]
-
-#     if not getattr(item.config.option, "adaptavist", False):
-#         # adaptavist reporting disabled: no need to proceed here
-#         return
-
-#     if pytest.test_result_data[item.fullname].get("done", False):
-#         # this item has been reported already within a meta block context (see below)
-#         return
-
-#     marker = get_marker(item, "testcase")
-#     if marker is not None:
-
-#         test_case_key = marker.kwargs["test_case_key"]
-#         test_step_key = marker.kwargs["test_step_key"]
-
-#         _, specs = get_item_name_and_spec(get_item_nodeid(item))
-#         create_report(test_case_key, test_step_key, call.stop - call.start, skip_status, report.passed, pytest.test_result_data[item.fullname], specs)
--- pytest_pretty_terminal/_pretty_terminal_reporter.py
+++ pytest_pretty_terminal/_pretty_terminal_reporter.py
@@ -1,28 +1,38 @@
-from _pytest.config import Config
+"""Terminal reporter module"""
+
+from typing import Optional, Tuple
+
 import pytest
+from _pytest.config import Config
 from _pytest.reports import TestReport
+from _pytest.terminal import TerminalReporter
+
 from ._helpers import get_item_name_and_spec, get_status_color
-from _pytest.python import Function
-from _pytest.runner import CallInfo
+
+
 class PrettyTerminalReporter:
-    """Terminal reporter class used for prettifying terminal output (also used for synchronization of xdist-worker nodes)."""
+    """
+    Terminal reporter class used for prettifying terminal output (also used for synchronization of xdist-worker nodes).
+
+    :param config: The pytest config object
+    """
 
     def __init__(self, config: Config):
-        """Constructor."""
+        """Constructor"""
 
         # dictionary to store final report
         pytest.report = {}
         self.config = config
-        self.tr = config.pluginmanager.getplugin("terminalreporter")
-        self.tr.showfspath = False
-
+        self.terminal_reporter: TerminalReporter = config.pluginmanager.getplugin("terminalreporter")
+        self.terminal_reporter.showfspath = False
 
     def pytest_runtest_logreport(self, report: TestReport):
-        """Empty log in pretty mode (will be done during execution, see calls of build_terminal_report())."""
 
         item_info = getattr(report, "item_info", {})
         user_properties = dict(report.user_properties)
-        worker_node_suffix = f" [{' -> '.join(filter(None, (report.node.gateway.id, item_info['atmcfg'].get('test_environment', None))))}]" if getattr(self.config.option, "dist", None) == "each" and getattr(report, "node") else ""
+        worker_node_suffix = f" [{' -> '.join(filter(None, (report.node.gateway.id, item_info['atmcfg'].get('test_environment', None))))}]" \
+            if getattr(self.config.option, "dist", None) == "each" and getattr(report, "node") \
+            else ""
 
         if item_info.get("atmcfg", None):
             pytest.project_key = item_info["atmcfg"].get("project_key", None)
@@ -45,29 +55,35 @@ class PrettyTerminalReporter:
         if report.when == "setup":
             if (getattr(self.config.option, "numprocesses", 0) or 0) < 2:
                 title, _ = get_item_name_and_spec(report.nodeid)
-                self.tr.line("")
-                self.tr.write_sep("-", title, bold=True)
-                self.tr.write_line(user_properties["docstr"] or "")
-                for k,v in user_properties.get("params", {}).items():
-                    self.tr.write_line(f"parameterization {k} {v}")
+                self.terminal_reporter.line("")
+                self.terminal_reporter.write_sep("-", title, bold=True)
+                self.terminal_reporter.write_line(user_properties["docstr"] or "")
+                for parameter, value in user_properties.get("params", {}).items():
+                    self.terminal_reporter.write_line(f"Parameterization: {parameter}={value}")
 
             if not report.skipped:
                 return
 
         if (getattr(self.config.option, "numprocesses", 0) or 0) > 1:
             title, _ = get_item_name_and_spec(report.nodeid)
-            self.tr.line("")
-            self.tr.write_sep("-", title + worker_node_suffix, bold=True)
-            self.tr.write_line(user_properties["docstr"] or "")
-            for k,v in report.user_properties.get("params", {}).items():
-                self.tr.write_line(f"parameterization {k} {v}")
+            self.terminal_reporter.line("")
+            self.terminal_reporter.write_sep("-", title + worker_node_suffix, bold=True)
+            self.terminal_reporter.write_line(user_properties["docstr"] or "")
+            for parameter, value in report.user_properties.get("params", {}).items():
+                self.terminal_reporter.write_line(f"Parameterization: {parameter}={value}")
 
-        self.tr.write_sep("-", bold=True)
-        fill = getattr(self.tr, "_tw").fullwidth - getattr(self.tr, "_width_of_current_line") - 1
-        self.tr.write_line(report.outcome.upper().rjust(fill), **get_status_color(report.outcome))
-    
+        self.terminal_reporter.write_sep("-", bold=True)
+        fill = getattr(self.terminal_reporter, "_tw").fullwidth - getattr(self.terminal_reporter, "_width_of_current_line") - 1
+        self.terminal_reporter.write_line(report.outcome.upper().rjust(fill), **get_status_color(report.outcome))
+
     @pytest.hookimpl(tryfirst=True)
-    def pytest_report_teststatus(self, report):
+    def pytest_report_teststatus(self, report: TestReport) -> Optional[Tuple[str, str, str]]:
+        """
+        Return result-category, shortletter and verbose word for status reporting.
+        In our case, the shortletter shall always be empty.
+
+        :param report: The report object whose status is to be returned
+        """
         if getattr(self.config.option, "pretty", False):
             outcome: str = report.outcome
             if report.when in ("collect", "setup", "teardown"):
@@ -76,4 +92,5 @@ class PrettyTerminalReporter:
                 elif not report.skipped:
                     outcome = ""
             return outcome, "", outcome.upper()
+        return None
         
